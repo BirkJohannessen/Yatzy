b89e0bb25e35415eb37c6d71947864f63ffe2854
591346 <591346@stud.hvl.no>
2022-03-23 16:48:45 +0100

    Terning ferdig

--- src/main/java/no/hvl/dat109/spill/Terning.java
+++ src/main/java/no/hvl/dat109/spill/Terning.java
@@ -13,21 +13,41 @@ public class Terning {
 
 	/**
 	 * Tilfeldig trill for treninger 
+	 * @return resultat på antall terninger etter fem trilte terninger
 	 */
 	
-	private static int[] Terning;
 	
 	public static int[] trillTerninger() {
-		Random ran = new Random();
-		for(int i = 0; i<5; i++) {
-			
-			Terning[i] = ran.nextInt(6);
-
+        int[] kast = new int[5];
+        for (int i = 0; i < 5; i++) {
+            kast[i] = (int) ((Math.random() * 6) + 1);
+        }
+        return kast;
+    }
+	
+	/**
+	 * Metode for resterende terninger som skal trilles
+	 * @param resterende
+	 * @return resultat på antall terninger som ikke ble ønskelig 
+	 */
 		
-		}
-		return Terning;
+	public static int[] trillResterendeTerning(int resterende) {
+        int[] kast = new int[resterende];
+        for (int i = 0; i < resterende; i++) {
+            kast[i] = (int) ((Math.random() * 6) + 1);
+        }
+        return kast;
+    }
 	}
-}
+	
+
+
+
+
+
+
+
+
 
 
 
