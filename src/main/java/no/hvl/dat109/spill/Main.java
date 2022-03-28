82d87fa933dc75e0eb25007e4e0b3d32f445a924
Birk Johannessen <69590435+BirkJohannessen@users.noreply.github.com>
2022-03-28 12:30:02 +0200

    Main ferdig

--- src/main/java/no/hvl/dat109/spill/Main.java
+++ src/main/java/no/hvl/dat109/spill/Main.java
@@ -3,7 +3,10 @@ package no.hvl.dat109.spill;
 public class Main {
 
 	public static void main(String[] args) {
+		Spiller[] spillere = Utils.hentSpillere();
+		Yatzy yatzy = new Yatzy(spillere);
 		
+		yatzy.spill();
 	}
 
 }
