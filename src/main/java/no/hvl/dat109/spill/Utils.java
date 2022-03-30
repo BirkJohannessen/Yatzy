67c80ee8a028a5ecda7c5358e958f5e449cf3670
Anders Lerang <69629274+andersler@users.noreply.github.com>
2022-03-30 13:36:33 +0200

    Update Utils.java

--- src/main/java/no/hvl/dat109/spill/Utils.java
+++ src/main/java/no/hvl/dat109/spill/Utils.java
@@ -4,15 +4,16 @@ import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
 
-/* @Author Anders Lerang
- * Utils klasse
+/**
+ * @Author Anders Lerang Utils klasse
  */
 public class Utils {
 
-	/*
-	 * Antall spillere
+	/**
+	 * @author ander Antall spillere
+	 * return returnerer antall spillere
 	 */
-	public static void antallSpillere() {
+	public static int antallSpillere() {
 
 		System.out.println("Hvor mange spillere?(2-6)");
 
@@ -31,30 +32,47 @@ public class Utils {
 		}
 
 		sc.close();
+		return ant;
 	}
 
-	public static void omKast(ArrayList<Integer> kopp) {
+	/**
+	 * 
+	 * @param kopp er terniger kastet fra spiller
+	 * @return ønsket terninger etter kast
+	 */
+	public static ArrayList<Integer> omKast(ArrayList<Integer> kopp) {
 
-		Scanner sc = new Scanner(System.in);
-		
-		System.out.print(" " + kopp + " ");
+		kopp.forEach(x -> System.out.print("[" + x + "]"));
 
 		System.out.println("\n");
+
+		System.out.println("Hvor mange terninger hvil du kaste?");
 		System.out.println("Hvilken terninger vil du kaste? (i index)");
+
+		Scanner ant = new Scanner(System.in);
+		int antall = ant.nextInt();
 		
-		while(sc.hasNext() == true) {
+		Scanner console = new Scanner(System.in);
+
+		System.out.println("Du kaster " + antall + " terninger vekk");
 		
-		int behold = sc.nextInt();
-		
-		kopp.remove(behold);
-		
-		kopp.forEach(x -> System.out.println(x));
-		
+
+		for (int i = 0; i < antall; i++) {
+
+			kopp.remove(console.nextInt());
+
+			kopp.forEach(x -> System.out.print("[" + x + "]"));
+
 		}
+
+		ArrayList<Integer> nyKopp = kopp;
+
+		return nyKopp;
 	}
 
-	/*
+	/**
 	 * Metode for å generere en unik spill id
+	 * return unik spillerid
 	 */
 	public static int genererSpillId() {
 
@@ -66,6 +84,5 @@ public class Utils {
 		return randomNumber;
 
 	}
-
 
 }
