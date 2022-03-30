c2415ec59340ef6ebb17d13f6023021068dffdf4
BirkJohannessen <Birk.Johannessen@hotmail.com>
2022-03-30 12:27:35 +0200

    maintest2

---
+++ src/main/java/no/hvl/dat109/spill/MainTest.java
@@ -0,0 +1,15 @@
+package no.hvl.dat109.spill;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MainTest {
+	public static void main(String[] args) {
+
+        ArrayList<Integer> gfg = new ArrayList<Integer>(
+                   Arrays.asList(2,2,3,1));
+
+       Utils.omKast(gfg);
+   }
+}
+
