3f2f1150de7ee7fb2a0405dafdeefa258344c485
591346 <69771520+591346@users.noreply.github.com>
2022-03-30 12:41:13 +0200

    terning ferdig

--- src/main/java/no/hvl/dat109/spill/Terning.java
+++ src/main/java/no/hvl/dat109/spill/Terning.java
@@ -1,6 +1,5 @@
 package no.hvl.dat109.spill;
 
-
 import java.util.ArrayList;
 /**
  * Definerer en terninger til Yatzy .
@@ -13,54 +12,46 @@ import java.util.Random;
 public class Terning {
 
 	/**
-	 * Tilfeldig trill for treninger 
+	 * Tilfeldig trill for treninger
+	 * 
 	 * @return resultat på antall terninger etter fem trilte terninger
 	 */
-	
-	
-	public static int[] trillTerninger() {
-		
-		
+
+	public static ArrayList<Integer> trillTerninger() {
+
 		ArrayList<Integer> trill = new ArrayList<Integer>(5);
-		
-        int[] kast = new int[5];
-        for (int i = 0; i < 5; i++) {
-            kast[i] = (int) ((Math.random() * 6) + 1);
-        }
-        for(int j: kast) {
-        	trill.add(j);
-        }
-        return kast;
-    }
-	
+
+		for (int i = 0; i < 5; i++) {
+			trill.add((int) ((Math.random() * 6) + 1));
+
+		}
+		return trill;
+	}
+
 	/**
 	 * Metode for resterende terninger som skal trilles
+	 * 
 	 * @param resterende
-	 * @return resultat på antall terninger som ikke ble ønskelig 
+	 * @return resultat på antall terninger som ikke ble ønskelig
 	 */
-		
-	public static int[] trillResterendeTerning(int resterende) {
-		ArrayList<Integer> trill = new ArrayList<Integer>(resterende);
-		
-        int[] kast = new int[resterende];
-        for (int i = 0; i < resterende; i++) {
-            kast[i] = (int) ((Math.random() * 6) + 1);
-        }
-        for(int j: kast) {
-        	trill.add(j);
-        }
-        return kast;
-    }
+
+	public static ArrayList<Integer> trillResterendeTerning(int resterende) {
+
+		ArrayList<Integer> terningVerdi = new ArrayList<Integer>(resterende);
+
+		if (resterende == 0) {
+
+			terningVerdi = null;
+		}
+
+		else {
+
+			for (int i = 0; i < resterende; i++) {
+
+				terningVerdi.add((int) ((Math.random() * 6) + 1));
+			}
+
+		}
+		return terningVerdi;
 	}
-	
-
-
-
-
-
-
-
-
-
-
-
+}
