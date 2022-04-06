c3c8515b1ac4ddc59f9993912f99f8054221d415
Andreas Wergeland 591122 <a.wergeland@hotmail.com>
2022-04-06 12:48:06 +0200

    Opprettet spillUtils

---
+++ src/main/java/no/hvl/dat109/spill/spillUtils.java
@@ -0,0 +1,21 @@
+package no.hvl.dat109.spill;
+
+import java.util.ArrayList;
+
+public class spillUtils {
+	
+	
+	public static int TreLike(ArrayList<Integer> Liste) {
+		
+		
+		return 0;
+		
+	}
+	
+	public static int FireLike(ArrayList<Integer> Liste) {
+		
+		
+		return 0;
+	}
+
+}
